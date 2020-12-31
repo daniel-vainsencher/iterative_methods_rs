9c539cbccec3a6c5f6446742c1839e2f2ad2deb3
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2020-12-31 16:24:38 +0200

    conjugate gradient types
    Compiles, but x does not update.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use ndarray::*;
+use streaming_iterator::*;
+
 struct FibonnacciIterable<T> {
     s0: T,
     s1: T,
@@ -9,6 +12,7 @@ impl FibonnacciIterable<f64> {
     }
     
 }
+
 impl Iterator for FibonnacciIterable<f64> {
     type Item = f64;
     fn next(&mut self) -> Option<Self::Item> {
@@ -20,10 +24,75 @@ impl Iterator for FibonnacciIterable<f64> {
     }
 }
 
-fn main() {
-    let mut fib = FibonnacciIterable::start(0.0, 1.0);
+fn fib_demo() {
+    let fib = FibonnacciIterable::start(0.0, 1.0);
 
     for n in fib.take(5) {
         println!("fib: {}", n)
     }
+}
+
+type S = f64;
+type M = Array2<S>;
+type V = Array1<S>;
+
+struct CGProblem {
+    a: M,
+    b: V,
+    x0: Option<V>,
+}
+
+struct CGIterable {
+    x: V,
+    r: V,
+    rs: f64,
+    rsprev: f64,
+    p: V,
+    ap: Option<V>,
+}
+
+impl CGIterable {
+    fn conjugate_gradient(p: CGProblem) -> CGIterable {
+        let x = match p.x0 {
+            None => ArrayBase::zeros(p.a.shape()[1]),
+            Some(init_x) => init_x
+        };
+        let r = p.b - p.a.dot(&x);
+        let rs = r.dot(&r); 
+        let p = r.clone();
+        
+        CGIterable {
+            x: x,
+            r: r,
+            rs: rs,
+            rsprev: 0.,
+            p: p,
+            ap: None
+        }
+    }
+
+}
+
+impl StreamingIterator for CGIterable {
+    type Item = CGIterable;
+    fn advance(&mut self) {
+        // TODO: implement
+    }
+    fn get(&self) -> Option<&Self::Item> {
+      Some(self)
+    }
+}
+
+fn main() {
+    fib_demo();
+    let a = arr2(&[[1.0, 0.5, 0.0],
+                   [0.5, 1.0, 0.0],
+                   [0.0, 0.5, 1.0]]);
+    let b = arr1(&[0., 1., 0.]);
+    let p = CGProblem {a: a, b: b, x0: None};
+    let mut cg_iter = CGIterable::conjugate_gradient(p).take(5);
+    while let Some(cgi) = cg_iter.next() {
+        println!("x is {}", cgi.x);
+    }
+    
 }
