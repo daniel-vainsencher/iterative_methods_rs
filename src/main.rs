f4f339d536a0c00cfef9fc94f65858c383998b74
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2021-01-01 03:00:23 -0800

    Add tee
    Implement it and use it for output.
    Also documentation improvements.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use ndarray::*;
 use streaming_iterator::*;
 
+/// State of Fibonacci iterator.
 struct FibonnacciIterable<T> {
     s0: T,
     s1: T,
@@ -32,6 +33,8 @@ impl Iterator for FibonnacciIterable<f64> {
 fn fib_demo() {
     let fib = FibonnacciIterable::start(0.0, 1.0);
 
+    // enumerate is a simple iterator adaptor annotating the results
+    // with their place in the sequence.
     for (i, n) in fib.enumerate().take(10) {
         println!("fib {} is {}", i, n)
     }
@@ -86,6 +89,7 @@ impl CGIterable {
 
 impl StreamingIterator for CGIterable {
     type Item = CGIterable;
+    /// Implementation of conjugate gradient iteration
     fn advance(&mut self) {
         let ap = self.a.dot(&self.p);
         let alpha = self.rs / self.p.dot(&ap);
@@ -101,6 +105,59 @@ impl StreamingIterator for CGIterable {
     }
 }
 
+/// Pass the values from the streaming iterator through, running a
+/// function on each for side effects.
+struct Tee<I, F> {
+    it: I,
+    f: F,
+}
+
+/*
+// TODO: For ideal convenience, this should be implemented inside the StreamingIterator trait.
+impl StreamingIterator<Item=T> {
+    fn tee<F>(self, f: F) -> Tee<Self, F>
+    where
+        Self: Sized,
+        F: Fn(&Self::Item)
+    {
+        Tee {
+            it: self,
+            f: f
+        }
+    }
+} */
+
+fn tee<I, F, T>(it: I, f: F) -> Tee<I, F>
+where
+    I: Sized + StreamingIterator<Item = T>,
+    F: Fn(&T),
+{
+    Tee { it: it, f: f }
+}
+
+impl<I, F> StreamingIterator for Tee<I, F>
+where
+    I: StreamingIterator,
+    F: FnMut(&I::Item),
+{
+    type Item = I::Item;
+
+    #[inline]
+    fn advance(&mut self) {
+        // The side effect happens exactly once for each new value
+        // generated.
+        self.it.advance();
+        if let Some(x) = self.it.get() {
+            (self.f)(x);
+        }
+    }
+
+    #[inline]
+    fn get(&self) -> Option<&I::Item> {
+        self.it.get()
+    }
+}
+
 /// Demonstrate usage and convergence of conjugate gradient as a streaming-iterator.
 fn cg_demo() {
     let a = arr2(&[[1.0, 0.5, 0.0], [0.5, 1.0, 0.0], [0.0, 0.5, 1.0]]);
@@ -110,15 +167,28 @@ fn cg_demo() {
         b: b,
         x0: None,
     };
-    let mut cg_iter = CGIterable::conjugate_gradient(p).take(10);
-    while let Some(cgi) = cg_iter.next() {
+    let cg_iter = CGIterable::conjugate_gradient(p)
+        // Upper bound the number of iterations
+        .take(20)
+        // Apply a quality based stopping condition; this relies on
+        // algorithm internals, requiring all state to be exposed and
+        // not just the result.
+        .take_while(|cgi| cgi.rsprev.sqrt() > 1e-6);
+    // Because tee is not part of the StreamingIterator trait, it
+    // cannot be chained as in the above. Note the side effect is
+    // applied exactly to every x produced above, the sequencce of
+    // which is not affected at all. This is just like applying a side
+    // effect inside the while loop, except we can compose multiple
+    // tee, each with its own effect.
+    let mut cg_print_iter = tee(cg_iter, |cgi| {
         println!(
             "||Ax - b ||_2 = {:.5}, for x = {:.4}, and Ax - b = {:.5}",
             cgi.rsprev.sqrt(),
             cgi.x,
             cgi.a.dot(&cgi.x) - &cgi.b,
         );
-    }
+    });
+    while let Some(_cgi) = cg_print_iter.next() {}
 }
 
 /// Call the different demos.
