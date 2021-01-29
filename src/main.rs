08722c343b0035b2c34463043e5870b36e90db5e
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2021-01-29 09:40:11 -0800

    Improve conjugate gradient and add tests for it (#10)
    Fixes:
    - Fix bug that cause convergence to sometimes fail
    - Fix bug in case initial answer was precise
    - Make CG finish before reaching divide by zero
    - fix display bug, show that norm is squared
    
    Tests:
    - Add quickcheck tests
    - Add manual tests that demonstrate some failure modes
    
    Misc:
    - make use and test "last" function.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 //! # iterative-methods
 //! A demonstration of the use of StreamingIterators and their adapters to implement iterative algorithms.
-
+extern crate eigenvalues;
+extern crate nalgebra as na;
+#[cfg(test)]
+extern crate quickcheck;
 use ndarray::*;
 use rand::{Rng, SeedableRng};
 use rand_pcg::Pcg64;
@@ -49,7 +52,8 @@ type M = ArcArray2<S>;
 type V = ArcArray1<S>;
 
 /// A linear system, ax-b=0, to be solved iteratively, with an optional initial solution.
-struct LinearSystem {
+#[derive(Clone, Debug)]
+pub struct LinearSystem {
     a: M,
     b: V,
     x0: Option<V>,
@@ -61,11 +65,12 @@ struct CGIterable {
     a: M,
     b: V,
     x: V,
+    alpha: S,
     r: V,
     rs: S,
     rsprev: S,
     p: V,
-    ap: Option<V>,
+    ap: V,
 }
 
 impl CGIterable {
@@ -76,18 +81,18 @@ impl CGIterable {
             Some(init_x) => init_x,
         };
         let r = problem.b.clone() - problem.a.dot(&x).view();
-        let rs = r.dot(&r);
-        let cgi_p = r.clone();
-
+        let p = r.clone();
+        let ap = problem.a.dot(&p).into_shared();
         CGIterable {
             a: problem.a,
             b: problem.b,
-            x: x,
-            r: r,
-            rs: rs,
-            rsprev: 0.,
-            p: cgi_p,
-            ap: None,
+            x,
+            alpha: 1.,
+            r,
+            rs: 1.,
+            rsprev: 1.,
+            p,
+            ap,
         }
     }
 }
@@ -96,17 +101,23 @@ impl StreamingIterator for CGIterable {
     type Item = CGIterable;
     /// Implementation of conjugate gradient iteration
     fn advance(&mut self) {
-        let ap = self.a.dot(&self.p).into_shared();
-        let alpha = self.rs / self.p.dot(&ap);
-        self.x += &(alpha * &self.p);
-        self.r -= &(alpha * &ap);
         self.rsprev = self.rs;
         self.rs = self.r.dot(&self.r);
-        self.p = (&self.r + &((&self.r / self.rsprev) * &self.p)).into_shared();
-        self.ap = Some(ap);
+        if self.rs.abs() <= std::f64::MIN_POSITIVE * 10. {
+            return;
+        }
+        self.p = (&self.r + &(&self.rs / self.rsprev * &self.p)).into_shared();
+        self.ap = self.a.dot(&self.p).into_shared();
+        self.alpha = self.rs / self.p.dot(&self.ap);
+        self.x += &(self.alpha * &self.p);
+        self.r -= &(self.alpha * &self.ap);
     }
     fn get(&self) -> Option<&Self::Item> {
-        Some(self)
+        if self.rsprev.abs() <= std::f64::MIN_POSITIVE * 10. {
+            None
+        } else {
+            Some(self)
+        }
     }
 }
 
@@ -163,15 +174,55 @@ where
     }
 }
 
-/// Demonstrate usage and convergence of conjugate gradient as a streaming-iterator.
-fn cg_demo() {
-    let a = rcarr2(&[[1.0, 0.5, 0.0], [0.5, 1.0, 0.0], [0.0, 0.5, 1.0]]);
-    let b = rcarr1(&[0., 1., 0.]);
-    let p = LinearSystem {
+pub fn solve_approximately(p: LinearSystem) -> V {
+    let solution = CGIterable::conjugate_gradient(p).take(200);
+    last(solution.map(|s| s.x.clone()))
+}
+
+pub fn show_progress(p: LinearSystem) {
+    let cg_iter = CGIterable::conjugate_gradient(p).take(50);
+    //.take_while(|cgi| cgi.rsprev.sqrt() > 1e-6);
+    let mut cg_print_iter = tee(cg_iter, |result| {
+        let res = result.a.dot(&result.x) - &result.b;
+        let res_norm = res.dot(&res);
+        println!(
+            "rs = {:.10}, ||Ax - b ||_2^2 = {:.5}, for x = {:.4}, and Ax - b = {:.5}",
+            result.rs,
+            res_norm,
+            result.x,
+            result.a.dot(&result.x) - &result.b,
+        );
+    });
+    while let Some(_cgi) = cg_print_iter.next() {}
+}
+
+pub fn make_3x3_psd_system_1() -> LinearSystem {
+    make_3x3_psd_system(
+        rcarr2(&[[1., 2., -1.], [0., 1., 0.], [0., 0., 1.]]),
+        rcarr1(&[0., 1., 0.]),
+    )
+}
+
+fn make_3x3_psd_system_2() -> LinearSystem {
+    make_3x3_psd_system(
+        rcarr2(&[[1.0, 0.5, 0.0], [0.5, 1.0, 0.5], [0.0, 0.5, 1.0]]),
+        rcarr1(&[0., 1., 0.]),
+    )
+}
+
+fn make_3x3_psd_system(m: M, b: V) -> LinearSystem {
+    let a = (m.t().dot(&m)).into_shared();
+    LinearSystem {
         a: a,
         b: b,
         x0: None,
-    };
+    }
+}
+
+/// Demonstrate usage and convergence of conjugate gradient as a streaming-iterator.
+fn cg_demo() {
+    let p = make_3x3_psd_system_2();
+    println!("a: \n{}", &p.a);
     let cg_iter = CGIterable::conjugate_gradient(p)
         // Upper bound the number of iterations
         .take(20)
@@ -186,12 +237,14 @@ fn cg_demo() {
     // side effect inside the while loop, except we can compose
     // multiple tee, each with its own effect.
     // TODO can this be fixed? see iterutils crate.
-    let step_by_cg_iter = step_by(cg_iter, 4);
+    let step_by_cg_iter = step_by(cg_iter, 2);
     let timed_cg_iter = time(step_by_cg_iter);
     let mut cg_print_iter = tee(timed_cg_iter, |TimedResult { result, duration }| {
+        let res = result.a.dot(&result.x) - &result.b;
+        let res_norm = res.dot(&res);
         println!(
-            "||Ax - b ||_2 = {:.5}, for x = {:.4}, and Ax - b = {:.5}; iteration duration {}μs",
-            result.rsprev.sqrt(),
+            "||Ax - b ||_2^2 = {:.5}, for x = {:.4}, and Ax - b = {:.5}; iteration duration {}μs",
+            res_norm,
             result.x,
             result.a.dot(&result.x) - &result.b,
             duration.as_nanos(),
@@ -215,6 +268,18 @@ where
 struct TimedResult<T> {
     result: T,
     duration: Duration,
+}
+
+pub fn last<I, T>(it: I) -> T
+where
+    I: StreamingIterator<Item = T>,
+    T: Sized + Clone,
+{
+    let last_some = it.fold(None, |_acc, i| Some((*i).clone()));
+    let last_item = last_some
+        .expect("StreamingIterator last expects at least one non-None element.")
+        .clone();
+    last_item
 }
 
 fn time<I, T>(it: I) -> TimedIterable<I, T>
@@ -473,7 +538,147 @@ fn main() {
 /// Unit Tests Module
 #[cfg(test)]
 mod tests {
+    use eigenvalues::algorithms::lanczos::HermitianLanczos;
+    use eigenvalues::SpectrumTarget;
+    use na::{DMatrix, DVector, Dynamic};
+    use ndarray::*;
+    use quickcheck::{quickcheck, TestResult};
+    #[test]
+    fn test_alt_eig() {
+        let dm = DMatrix::from_row_slice(3, 3, &[3.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 2.0]);
+        println!("dm: {}", dm);
+
+        let high = HermitianLanczos::new(dm.clone(), 3, SpectrumTarget::Highest)
+            .unwrap()
+            .eigenvalues[(0, 0)];
+        println!("high: {}", &high);
+        assert!((high - 3.).abs() < 0.001);
+    }
+
+    fn eigvals(m: &M) -> Result<DVector<f64>, String> {
+        let shape = m.shape();
+        let h = shape[0];
+        let w = shape[1];
+        assert_eq!(h, w);
+        let elems = m.reshape(h * w).to_vec();
+        let dm = na::DMatrix::from_vec_generic(Dynamic::new(h), Dynamic::new(w), elems);
+        Ok(
+            HermitianLanczos::new(dm.clone(), 3, SpectrumTarget::Highest)?
+                .eigenvalues
+                .clone(),
+        )
+    }
+
+    fn test_arbitrary_3x3_psd(vs: Vec<u16>, b: Vec<u16>) -> TestResult {
+        // Currently require dimension 3
+        if b.len().pow(2) != vs.len() || b.len() != 3 {
+            return TestResult::discard();
+        }
+        let vs = rcarr1(&vs).reshape((3, 3)).map(|i| *i as f64).into_shared();
+        let b = rcarr1(&b).map(|i| *i as f64).into_shared();
+        let p = make_3x3_psd_system(vs, b);
+        // Decomposition should always succeed as p.a is p.s.d. by
+        // construction; if not this is a bug in the test.
+        let eigvals = eigvals(&p.a).expect(&format!("Failed to compute eigenvalues for {}", &p.a));
+
+        // Ensure A is positive definite with no extreme eigenvalues.
+        if !eigvals.iter().all(|ev| &1e-9 < ev && ev < &1e9) {
+            return TestResult::discard();
+        }
+
+        println!("eigvals of a: {}", eigvals);
+
+        println!("a: {}", p.a);
+        println!("b: {}", p.b);
+        let x = solve_approximately(p.clone());
+        let res = p.a.dot(&x) - &p.b;
+        let res_square_norm = res.dot(&res);
+        println!("x: {}", x);
+        show_progress(p.clone());
+        //
+        TestResult::from_bool(res_square_norm < 1e-40)
+    }
+
+    quickcheck! {
+        /// Test that we obtain a low precision solution for small p.s.d.
+        /// matrices of not-too-large numbers.
+        fn prop(vs: Vec<u16>, b: Vec<u16>) -> TestResult {
+            test_arbitrary_3x3_psd(vs, b)
+        }
+    }
+
     use super::*;
+
+    #[test]
+    fn test_last() {
+        let v = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let iter = convert(v.clone());
+        assert!(last(iter) == 9);
+    }
+
+    #[test]
+    #[should_panic(expected = "StreamingIterator last expects at least one non-None element.")]
+    fn test_last_fail() {
+        let v: Vec<u32> = vec![];
+        last(convert(v.clone()));
+    }
+
+    #[test]
+    fn cg_simple_test() {
+        let p = make_3x3_psd_system_1();
+        println!("Problem is: {:?}", p);
+        show_progress(p.clone());
+        let x = solve_approximately(p.clone());
+        let r = p.a.dot(&x) - p.b;
+        println!("Residual is: {}", r);
+        let res_square_norm = r.dot(&r);
+        println!("Residual squared norm is: {}", res_square_norm);
+        assert!(res_square_norm < 1e-10);
+    }
+
+    #[test]
+    fn cg_simple_passed() {
+        let p = LinearSystem {
+            a: rcarr2(&[[1.0, 0.5, 0.0], [0.5, 1.0, 0.5], [0.0, 0.5, 1.0]]),
+            b: rcarr1(&[0.0, 1., 0.]),
+            x0: None,
+        };
+
+        println!("Problem is: {:?}", p);
+        show_progress(p.clone());
+        let x = solve_approximately(p.clone());
+        let r = p.a.dot(&x) - p.b;
+        println!("Residual is: {}", r);
+        let res_square_norm = r.dot(&r);
+        println!("Residual squared norm is: {}", res_square_norm);
+        assert!(res_square_norm < 1e-10);
+    }
+
+    #[test]
+    fn cg_zero_x() {
+        let result = test_arbitrary_3x3_psd(vec![0, 0, 1, 1, 0, 0, 0, 1, 0], vec![0, 0, 0]);
+        assert!(!result.is_failure());
+        assert!(!result.is_error());
+    }
+
+    #[test]
+    fn cg_rank_one_v() {
+        // This test is currently discarded by test_arbitrary_3x3_pd
+        let result = test_arbitrary_3x3_psd(vec![0, 0, 0, 0, 0, 0, 1, 43, 8124], vec![0, 0, 1]);
+        assert!(!result.is_failure());
+        assert!(!result.is_error());
+    }
+
+    #[test]
+    fn cg_horribly_conditioned() {
+        // This example is very highly ill-conditioned:
+        // eigvals: [2904608166.992541+0i, 0.0000000010449559455574797+0i, 0.007460513747178893+0i]
+        // therefore is currently discarded by the upper bound on eigenvalues.
+        let result =
+            test_arbitrary_3x3_psd(vec![0, 0, 0, 0, 0, 1, 101, 4654, 53693], vec![0, 0, 6]);
+        assert!(!result.is_failure());
+        assert!(!result.is_error());
+    }
 
     #[test]
     fn step_by_test() {
