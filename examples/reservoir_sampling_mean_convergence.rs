f86da96b192cb046634409a8f9a183dff7b0e7d9
Daniel Fox <danielfox@thoughtstoodefinite.com>
2021-03-24 22:21:05 -0400

    Complete calculating reservoir and stream means and plot. Plotting now works. Revise docs.

---
+++ examples/reservoir_sampling_mean_convergence.rs
@@ -0,0 +1,76 @@
+use crate::utils;
+use iterative_methods::*;
+use std::collections::HashMap;
+use std::process::Command;
+use streaming_iterator::*;
+
+/// Create a stream of data, write the full stream and a
+/// sequence of reservoir samples to yaml files. The stream 
+/// uses enumerated samples in order to track how much of the 
+/// stream has been used in each reservoir.
+fn reservoir_sampling_mean_convergence_for_step() -> std::io::Result<()> {
+    // Streamline up error handling
+    let stream_size: usize = 10_i32.pow(3) as usize;
+    let capacity: usize = 50;
+    let step: usize = 1;
+    let num_bins = 3usize;
+    let mut parameters: HashMap<&str, usize> = HashMap::new();
+    parameters.insert("stream_size", stream_size);
+    parameters.insert("capacity", capacity);
+    parameters.insert("step", step);
+    parameters.insert("num_bins", num_bins);
+    println!(
+        "The test uses a stream of size {:#?} and a reservoir capacity of {:#?}.",
+        stream_size, capacity
+    );
+
+    // Create a copy of the stream to be written to yaml:
+    let population_file = "./target/debug/examples/population.yaml";
+    let stream_to_yaml = utils::generate_step_stream(stream_size, capacity, 0, 1);
+    let stream_to_yaml = convert(stream_to_yaml);
+    let stream_to_yaml = enumerate(stream_to_yaml);
+    let mut stream_to_yaml = write_yaml_documents(stream_to_yaml, population_file.to_string())
+        .expect("Create File and initialize yaml iter failed.");
+    while let Some(_) = stream_to_yaml.next() {}
+
+    // Create another copy of the stream to perform reservoir sampling and write to yaml:
+    let stream = utils::generate_step_stream(stream_size, capacity, 0, 1);
+    let stream = convert(stream);
+    let stream = enumerate(stream);
+    let res_iter = reservoir_iterable(stream, capacity, None);
+    let res_iter = step_by(res_iter, step);
+    let reservoir_samples_file = "./target/debug/examples/reservoirs.yaml";
+    // Write data to file for visualization.
+    let mut res_to_yaml = write_yaml_documents(res_iter, reservoir_samples_file.to_string())
+        .expect("Create File and initialize yaml iter failed.");
+    // num_res is used in the python script for visualizations to initialize the size of the array that will hold that data to visualize.
+    let mut num_res = 0;
+    while let Some(_item) = res_to_yaml.next() {
+        num_res += 1
+    }
+    parameters.insert("num_res", num_res);
+    let parameters_file_path = "./visualizations_python/parameters.yaml";
+    utils::write_parameters_to_yaml(parameters, parameters_file_path)?;
+    Ok(())
+}
+
+fn make_animations_in_python() -> std::io::Result<()> {
+    let output = Command::new("python3")
+        .arg("./visualizations_python/reservoir_sampling_mean_convergence.py")
+        .output()?;
+    if !output.status.success() {
+        println!(
+            "Running reservoir_sampling_mean_convergence.py did not succeed. Error: \n\n{:#?}\n\n",
+            output
+        );
+    } else {
+        println!("{:#?}", output);
+    };
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    reservoir_sampling_mean_convergence_for_step()?;
+    make_animations_in_python()?;
+    Ok(())
+}
---
+++ visualizations_python/reservoir_sampling_mean_convergence.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import yaml
+from yaml import CLoader
+import plotly.graph_objects as go
+import plotly.express as px
+import os
+from operator import itemgetter
+
+parameters = {}
+with open("./visualizations_python/parameters.yaml") as parameters_file:
+    
+    parameters = yaml.load(parameters_file, Loader = CLoader)
+
+with open("./target/debug/examples/reservoirs.yaml") as res_file, open("./target/debug/examples/population.yaml") as pop_file:
+
+    reservoirs = yaml.load_all(res_file, Loader=CLoader)
+    population = yaml.load_all(pop_file, Loader = CLoader)
+    population = [value for i, value in population]
+    population = np.array(population, dtype = float)
+    print(population)
+    arr = np.full((parameters['num_res'],3),0, dtype = float)
+    for i, res in enumerate(reservoirs):
+        print("res:", res)
+        ind = max(map(itemgetter(0), res))
+        arr[i,0] = ind
+        arr[i,1] = np.mean(np.array(res, dtype = float), axis = 0)[1]
+        arr[i,2] = np.mean(population[:ind+1]) # the mean of the population from which the current reservoir is drawn
+    # print("the array of indices and means:\n", arr)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = arr[:,0], y = arr[:,1], name = "Reservoir Means", mode = "lines+markers"))
+    fig.add_trace(go.Scatter(x = arr[:,0], y = arr[:,2], name = "Stream Means", mode = "lines+markers"))
+    fig.update_layout(title = f"Reservoir and Stream Means. Stream Size={parameters['stream_size']}, Capacity={parameters['capacity']}, Number of Reservoirs={parameters['num_res']}")
+    # fig.show()
+
+    # To export a still image:
+    if not os.path.exists("visualizations"):
+        os.mkdir("visualizations")
+    fig.write_image("visualizations/reservoir_and_stream_means.png")
+
+
+    
+    
+
+
+
+
