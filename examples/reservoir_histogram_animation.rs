2451e53ad301caf11734541189c7255d6f395fc9
Daniel Fox <danielfox@thoughtstoodefinite.com>
2021-03-29 17:05:55 -0400

    Add example with animations of reservoir histograms.

---
+++ examples/reservoir_histogram_animation.rs
@@ -0,0 +1,85 @@
+use crate::utils;
+use iterative_methods::*;
+use std::collections::HashMap;
+use std::process::Command;
+use streaming_iterator::*;
+
+/// Write the full stream and a sequence of reservoir samples
+/// to yaml files. The stream
+/// is enumerated in order to track how much of the
+/// stream has been used in each reservoir.
+fn reservoir_histogram_animation() -> std::io::Result<()> {
+    // Streamline up error handling
+    let stream_size: usize = 10_i32.pow(05) as usize;
+    // let num_of_initial_values = stream_size / 2;
+    let capacity: usize = 200;
+    let mut parameters: HashMap<&str, String> = HashMap::new();
+    parameters.insert("stream_size", stream_size.to_string());
+    parameters.insert("capacity", capacity.to_string());
+    println!(
+        "The test uses a stream of size {:#?} and a reservoir capacity of {:#?}.",
+        stream_size, capacity
+    );
+    let sigma = 0.25f64;
+    parameters.insert("sigma",sigma.to_string());
+    // Generate the data to use
+    let mut stream_vec = utils::generate_stream_from_normal_distribution(stream_size, 0.25f64, sigma);
+    let mut stream_vec_end = utils::generate_stream_from_normal_distribution(2*stream_size, 0.75f64, sigma);
+    stream_vec.append(&mut stream_vec_end);
+
+    // Create a copy of the stream to be written to yaml:
+    let population_file = "./target/debug/examples/population_for_histogram.yaml";
+    parameters.insert("population_file", population_file.to_string());
+    // let stream_to_yaml = utils::generate_step_stream(stream_size, num_of_initial_values, 0, 1);
+    let population_stream = stream_vec.clone();
+    let population_stream = population_stream.iter();
+    let population_stream = convert(population_stream);
+    let population_stream = enumerate(population_stream);
+    let mut population_stream = write_yaml_documents(population_stream, population_file.to_string())
+        .expect("Create File and initialize yaml iter failed.");
+    while let Some(_) = population_stream.next() {}
+
+    // Create another copy of the stream to perform reservoir sampling and write to yaml:
+    // let stream = utils::generate_step_stream(stream_size, num_of_initial_values, 0, 1);
+    let stream = stream_vec.iter();
+    let stream = convert(stream);
+    let stream = enumerate(stream);
+    let res_iter = reservoir_iterable(stream, capacity, None);
+    let res_iter = step_by(res_iter, 20);
+    let reservoir_samples_file = "./target/debug/examples/reservoirs_for_histogram.yaml";
+    parameters.insert("reservoir_samples_file", reservoir_samples_file.to_string());
+    // Write data to file for visualization.
+    let mut res_to_yaml = write_yaml_documents(res_iter, reservoir_samples_file.to_string())
+        .expect("Create File and initialize yaml iter failed.");
+    // num_res is used in the python script for visualizations to initialize the size of the array that will hold that data to visualize.
+    let mut num_res = 0;
+    while let Some(_item) = res_to_yaml.next() {
+        num_res += 1
+    }
+    parameters.insert("num_res", num_res.to_string());
+    let parameters_file_path = "./visualizations_python/parameters_for_histogram.yaml";
+    parameters.insert("parameters_file_path", parameters_file_path.to_string());
+    utils::write_parameters_to_yaml(parameters, parameters_file_path)?;
+    Ok(())
+}
+
+fn make_animations_in_python() -> std::io::Result<()> {
+    let output = Command::new("python3")
+        .arg("./visualizations_python/reservoir_histogram_animation.py")
+        .output()?;
+    if !output.status.success() {
+        println!(
+            "Running reservoir_histogram_animation.py did not succeed. Error: {:#?}",
+            output
+        );
+    } else {
+        println!("{:#?}", output);
+    };
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    reservoir_histogram_animation()?;
+    make_animations_in_python()?;
+    Ok(())
+}
---
+++ visualizations_python/reservoir_histogram_animation.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+import yaml
+import plotly.graph_objects as go
+import plotly.express as px
+import os
+from operator import itemgetter
+
+
+def cleanup_test_files(files_to_remove: list):
+    """
+    The Rust code panics if the files it writes to already exists. This fn
+    removes them after they have been used so that the example can be run again.
+    It also prevents files used only for the tests from accumulating in the repo.
+    """
+    for file in files_to_remove:
+        os.remove(file)
+
+
+files_to_remove = []
+parameters = {}
+with open("./visualizations_python/parameters_for_histogram.yaml") as parameters_file:
+
+    parameters = yaml.load(parameters_file, Loader=yaml.CLoader)
+
+files_to_remove.append(parameters["parameters_file_path"])
+
+with open(parameters["reservoir_samples_file"]) as res_file, open(
+    parameters["population_file"]
+) as pop_file:
+
+    capacity = int(parameters["capacity"])
+    num_res = int(parameters["num_res"])
+    print("capacity, num_res:", capacity, num_res)
+    reservoirs = yaml.load_all(res_file, Loader=yaml.CLoader)
+    population = yaml.load_all(pop_file, Loader=yaml.CLoader)
+    population = [value for i, value in population]
+    population = np.array(population, dtype=float)
+    arr = np.full((capacity * num_res, 2), 0, dtype=float)
+    for i, res in enumerate(reservoirs):
+        ind = max(map(itemgetter(0), res))
+        res = list(map(itemgetter(1), res))
+        arr[i * capacity : (i + 1) * capacity, 0] = np.full(capacity, ind)
+        arr[i * capacity : (i + 1) * capacity, 1] = np.array(res)
+
+    sigma = float(parameters["sigma"])
+    xm = np.min(population) - 0.2
+    xM = np.max(population) + 0.2
+    ym = 0
+    yM = 1 / np.sqrt(2 * np.pi * sigma)
+    num_bins = 20
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Histogram(
+            x=population,
+            nbinsx=num_bins,
+            histnorm="probability",
+            marker_color="#EB89B5",
+            opacity=0.75,
+            name="Population Distribution",
+        )
+    )
+    fig.add_trace(
+        go.Histogram(
+            x=population,
+            nbinsx=num_bins,
+            histnorm="probability",
+            marker_color="#EB89B5",
+            opacity=0.75,
+            name="Population Distribution",
+        )
+    )
+    # Note: the reservoir is constant for a long time, so animation begins well into the sequence of reservoirs
+    fig.frames = [
+        go.Frame(
+            data=[
+                go.Histogram(
+                    x=arr[i * capacity : (i + 1) * capacity, 1],
+                    nbinsx=num_bins,
+                    histnorm="probability",
+                    marker_color="#330C73",
+                    opacity=0.75,
+                    name="Reservoir Distribution",
+                )
+            ]
+        )
+        for i in range(num_res)
+    ]
+    fig.layout = go.Layout(
+        xaxis=dict(range=[xm, xM], autorange=False, zeroline=False),
+        yaxis=dict(range=[ym, yM], autorange=False, zeroline=False),
+        title_text="Drifting Distribution",
+        hovermode="closest",
+        updatemenus=[
+            dict(
+                type="buttons",
+                buttons=[
+                    dict(label="Play", method="animate", args=[None, {"frame": {"duration": 50, "redraw": False},}]),
+                ],
+            )
+        ],
+        barmode="group",
+        bargroupgap=0.1,
+    )
+    fig.update_layout(transition_duration=0)
+
+    # Overlay both histograms
+    # fig.update_layout(barmode="overlay")
+    # Reduce opacity to see both histograms
+    # fig.update_traces(opacity=0.5)
+
+    # To export:
+    if not os.path.exists("visualizations"):
+        os.mkdir("visualizations")
+    fig.write_html("visualizations/reservoir_histogram_animation.html")
+
+files_to_remove.append(parameters["reservoir_samples_file"])
+files_to_remove.append(parameters["population_file"])
+
+
+cleanup_test_files(files_to_remove)
