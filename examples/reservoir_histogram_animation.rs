e3fb0149784a8f6dfd99238d3bbc83b320746e5c
Daniel Fox <danielfox@thoughtstoodefinite.com>
2021-04-11 13:44:39 -0400

    Delete yaml files of data prior to running example rather than after.
    Revise handling of parameters passing and file name passing.
    
    parameters hashmap now has String keys instead of &str.

--- examples/reservoir_histogram_animation.rs
+++ examples/reservoir_histogram_animation.rs
@@ -4,40 +4,37 @@ use std::cmp;
 use std::collections::HashMap;
 use std::process::Command;
 use streaming_iterator::*;
+use std::fs;
 
 /// Write the full stream and a sequence of reservoir samples
 /// to yaml files. The stream
 /// is enumerated in order to track how much of the
 /// stream has been used in each reservoir.
-fn reservoir_histogram_animation() -> Result<Vec<String>, std::io::Error> {
+fn reservoir_histogram_animation(file_list: Vec<String>) -> std::io::Result<()>  {
     // Streamline up error handling
     let stream_size: usize = 10_i32.pow(04) as usize;
     let num_initial_values = stream_size / 4;
     let num_final_values = 3 * stream_size / 4;
     let capacity: usize = 500;
     let num_bins: usize = 50;
-    let mut parameters: HashMap<&str, String> = HashMap::new();
-    // Define file paths for yaml data
-    let population_file = "./target/debug/examples/population_for_histogram.yaml";
-    let reservoir_samples_file = "./target/debug/examples/reservoirs_for_histogram.yaml";
-    let parameters_file_path = "./visualizations_python/parameters_for_histogram.yaml";
-    let reservoir_means_file = "./target/debug/examples/reservoir_means.yaml";
-    let file_list = vec![
-        population_file.to_string(),
-        reservoir_samples_file.to_string(),
-        parameters_file_path.to_string(),
-        reservoir_means_file.to_string(),
-    ];
+    let mut parameters: HashMap<String, String> = HashMap::new();
 
-    parameters.insert("stream_size", stream_size.to_string());
-    parameters.insert("num_initial_values", num_initial_values.to_string());
-    parameters.insert("num_final_values", num_final_values.to_string());
-    parameters.insert("capacity", capacity.to_string());
-    parameters.insert("population_file", population_file.to_string());
-    parameters.insert("reservoir_samples_file", reservoir_samples_file.to_string());
-    parameters.insert("parameters_file_path", parameters_file_path.to_string());
-    parameters.insert("reservoir_means_file", reservoir_means_file.to_string());
-    parameters.insert("num_bins", num_bins.to_string());
+    parameters.insert("stream_size".to_string(), stream_size.to_string());
+    parameters.insert("num_initial_values".to_string(), num_initial_values.to_string());
+    parameters.insert("num_final_values".to_string(), num_final_values.to_string());
+    parameters.insert("capacity".to_string(), capacity.to_string());
+    parameters.insert("num_bins".to_string(), num_bins.to_string());
+    
+
+    for file in file_list {
+        parameters.insert(file.clone(), file);
+    }
+
+    parameters.insert("population_file".to_string(), "./target/debug/examples/population_for_histogram.yaml".to_string());
+    parameters.insert("reservoir_samples_file".to_string(), "./target/debug/examples/reservoirs_for_histogram.yaml".to_string());
+    parameters.insert("parameters_file_path".to_string(), "./visualizations_python/parameters_for_histogram.yaml".to_string());
+    parameters.insert("reservoir_means_file".to_string(), "./target/debug/examples/reservoir_means.yaml".to_string());
+    
     println!(
         "The test uses a stream of size {:#?} and a reservoir capacity of {:#?}.",
         stream_size, capacity
@@ -45,7 +42,7 @@ fn reservoir_histogram_animation() -> Result<Vec<String>, std::io::Error> {
     let sigma = 0.15f64;
     let mean_initial = 0.25f64;
     let mean_final = 0.75f64;
-    parameters.insert("sigma", sigma.to_string());
+    parameters.insert("sigma".to_string(), sigma.to_string());
 
     // Generate the data as a vec
     let mut stream_vec =
@@ -58,11 +55,11 @@ fn reservoir_histogram_animation() -> Result<Vec<String>, std::io::Error> {
     let stream = stream_vec.iter();
     let stream = convert(stream);
     let stream = enumerate(stream);
-    let stream = write_yaml_documents(stream, population_file.to_string())
+    let stream = write_yaml_documents(stream, parameters["population_file"].to_string())
         .expect("Create File and initialize yaml iter failed.");
     let stream = reservoir_iterable(stream, capacity, None);
     let stream = step_by(stream, 20);
-    let stream = write_yaml_documents(stream, reservoir_samples_file.to_string())
+    let stream = write_yaml_documents(stream, parameters["reservoir_samples_file"].to_string())
         .expect("Create File and initialize yaml iter failed.");
     let reservoir_mean_and_max_index = |reservoir: &Vec<Numbered<&f64>>| -> Numbered<f64> {
         let mut max_index = 0i64;
@@ -80,16 +77,17 @@ fn reservoir_histogram_animation() -> Result<Vec<String>, std::io::Error> {
         }
     };
     let stream = stream.map(reservoir_mean_and_max_index);
-    let mut stream = write_yaml_documents(stream, reservoir_means_file.to_string())
+    let mut stream = write_yaml_documents(stream, parameters["reservoir_means_file"].to_string())
         .expect("Create File and initialize yaml iter failed.");
     // num_res is used in the python script for visualizations to initialize the size of the array that will hold that data to visualize.
     let mut num_res = 0i64;
     while let Some(_item) = stream.next() {
         num_res += 1;
     }
-    parameters.insert("num_res", num_res.to_string());
-    utils::write_parameters_to_yaml(parameters, parameters_file_path)?;
-    Ok(file_list)
+    parameters.insert("num_res".to_string(), num_res.to_string());
+    let param_file_path = parameters["parameters_file_path"].clone();
+    utils::write_parameters_to_yaml(parameters, &param_file_path)?;
+    Ok(())
 }
 
 fn make_initial_final_histograms_in_python() -> std::io::Result<()> {
@@ -137,25 +135,33 @@ fn make_animations_in_python() -> std::io::Result<()> {
     Ok(())
 }
 
-fn remove_yaml_files(file_list: Vec<String>) -> std::io::Result<()> {
-    for file in file_list {
-        let output = Command::new("rm").arg(file).output()?;
-        if !output.status.success() {
-            println!(
-                "Running reservoir_histogram_animation.py did not succeed. Error: {:#?}",
-                output
-            );
-        }
+fn remove_yaml_files() -> Result<Vec<String>, std::io::Error> {
+    // Define file paths for yaml data
+    let population_file = "./target/debug/examples/population_for_histogram.yaml";
+    let reservoir_samples_file = "./target/debug/examples/reservoirs_for_histogram.yaml";
+    let parameters_file_path = "./visualizations_python/parameters_for_histogram.yaml";
+    let reservoir_means_file = "./target/debug/examples/reservoir_means.yaml";
+
+    let file_list = vec![
+        population_file.to_string(),
+        reservoir_samples_file.to_string(),
+        parameters_file_path.to_string(),
+        reservoir_means_file.to_string(),
+    ];
+    for file in file_list.iter() {
+        if let Ok(_) = fs::metadata(file) {
+            Command::new("rm").arg(file).output()?;
+        } 
     }
-    Ok(())
+    Ok(file_list)
 }
 
 fn main() -> std::io::Result<()> {
-    let file_list = reservoir_histogram_animation()?;
+    let file_list = remove_yaml_files()?;
+    reservoir_histogram_animation(file_list)?;
     println!("Data is written to yaml files.");
     make_initial_final_histograms_in_python()?;
     make_reservoir_means_plot_in_python()?;
     make_animations_in_python()?;
-    remove_yaml_files(file_list)?;
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -134,7 +134,7 @@ pub fn expose_w(count: &f64) -> f64 {
 /// Utility functions for writing data to yaml, including for visualizations
 ///
 /// The order of the parameters is not controlled.
-pub fn write_parameters_to_yaml<T>(params: HashMap<&str, T>, file_path: &str) -> std::io::Result<()>
+pub fn write_parameters_to_yaml<T>(params: HashMap<String, T>, file_path: &str) -> std::io::Result<()>
 where
     T: std::string::ToString,
 {
