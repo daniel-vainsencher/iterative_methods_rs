57f37f98a771c943cbc56c88d5b33b26f5adaa32
Daniel Fox <danielfox@thoughtstoodefinite.com>
2021-04-14 17:45:07 -0400

    Error messages for python scripts now give instructions for how to run the example without visualizations.

--- visualizations_python/reservoir_means.py
+++ visualizations_python/reservoir_means.py
@@ -8,7 +8,10 @@ try:
 except ModuleNotFoundError as error:
     print(f"ModuleNotFoundError:", error, """\n 
         You do not have the Python modules needed to generate the visualizations for this example.
-        You can install them using the following steps:
+        You can install the dependencies or run the example without visualizations.
+
+        To install the dependencies use the following steps:
+
             0) If you don't already have it, install Python3 following the instructions at https://www.python.org/downloads/.
             
             1) Install pip and virtual env according to the instructions here:
@@ -25,6 +28,10 @@ except ModuleNotFoundError as error:
             `$ pip install -r ./visualizations_python/requirements.txt`
 
             5) Rerun the examples.
+
+        To run the example without visualizations, add the command line argument 'false':
+
+            `$ ./target/debug/examples/reservoir_histogram_animation false`
         """)
     sys.exit(1)
 
